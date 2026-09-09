b173830958195ffea1d90b91a6c7dbe4e0647ab5
rasoulmoradi849-lab <rasoulmoradi849@gmail.com>
2026-09-09 06:26:09 -0700

    Update visualization.js

--- demo/js/visualization.js
+++ demo/js/visualization.js
@@ -1,25 +1,66 @@
 function visualizeMineral(output,T,t){
 
 
-let mineral =
-output.output_1;
+console.log("Visualization output:",output);
+
+
+// Get ONNX output
+
+let prediction =
+output.prediction.cpuData;
+
+
+
+console.log(
+"Prediction length:",
+prediction.length
+);
+
+
+
+// Number of predicted fields
+
+let nFields = 13;
+
+let nx = 100;
+let ny = 100;
+
+
+
+// Select Fo90 field
+
+let fieldIndex = 0;
 
 
 
 let field=[];
 
 
-for(let j=0;j<100;j++){
+
+for(let j=0;j<ny;j++){
+
 
     let row=[];
 
-    for(let i=0;i<100;i++){
+
+    for(let i=0;i<nx;i++){
+
+
+        let index =
+        fieldIndex*nx*ny
+        +
+        j*nx
+        +
+        i;
+
 
         row.push(
-            mineral[j*100+i]
+            prediction[index]
         );
 
+
     }
+
 
     field.push(row);
 
@@ -27,7 +68,12 @@ for(let j=0;j<100;j++){
 
 
 
-let data=[
+
+Plotly.newPlot(
+
+"mineralPlot",
+
+[
 
 {
 
@@ -38,32 +84,30 @@ type:"heatmap",
 colorscale:"Viridis",
 
 colorbar:{
-title:"Fo90 fraction"
+title:"Fo90"
 }
 
+
 }
 
-];
+],
 
-
-
-Plotly.newPlot(
-"mineralPlot",
-    
-data,
 
 {
+
 
 title:
 "Fo90 Prediction | T="
 +T+
-" °C , t="
+" °C | t="
 +t+
 " h",
+
 
 xaxis:{
 title:"x (cm)"
 },
+
 
 yaxis:{
 title:"y (cm)"
@@ -71,6 +115,7 @@ title:"y (cm)"
 
 
 }
+
 
 );
 
