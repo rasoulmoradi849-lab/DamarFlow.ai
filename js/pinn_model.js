3119cc7da416bb57f3114b2e8c91732ea39aab94
rasoulmoradi849-lab <rasoulmoradi849@gmail.com>
2026-09-09 08:13:59 -0700

    Update pinn_model.js

--- js/pinn_model.js
+++ js/pinn_model.js
@@ -13,7 +13,7 @@ let pinn_session = null;
 // ============================================================
 
 const PINN_MODEL_PATH =
-    "demo/model/serpentinization_pinn.onnx";
+    "demo/model/serpentinization_";
 
 
 // ============================================================
